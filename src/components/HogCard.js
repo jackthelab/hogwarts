13e25e3ee5e0fda6a44915d414b95a78bc18c8e0
Jacob Hampton <jhampton792@gmail.com>
2021-03-19 10:06:58 -0500

    Took out unnecessary lines

--- src/components/HogCard.js
+++ src/components/HogCard.js
@@ -1,9 +1,5 @@
 import React from 'react'
 
-// function imageUrl(hogName) {
-//     return `/src/hog-imgs/${hogName.split(' ').join('_')}.jpg`
-// }
-
 const plzWorkPigPic = (hogName) => {
     let imgName = hogName.split(" ").join("_").toLowerCase()
     let pigImgs = require(`../hog-imgs/${imgName}.jpg`)
@@ -15,8 +11,6 @@ const HogCard = ({ hog }) => {
         <div className="pigTile">
             <h5>{ hog.name }</h5>
             <img src={plzWorkPigPic(hog.name)} className="minPigTile" alt="" />
-            {/* <img src={imageUrl(hog.name)} alt=""/> */}
-            {/* <img src="../hog-imgs/babe.jpg" alt=''/> */}
         </div>
     )
 }
